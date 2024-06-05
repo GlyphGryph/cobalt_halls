8287261bf49b1838a89a035777561d76717091f0
Kevin McKayven <kmckayve@akamai.com>
2024-06-05 10:14:48 -0400

    Methods for moving character between rooms and seeing their descriptions.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,12 @@
 class Character < ApplicationRecord
   belongs_to :room
+
+  def move(destination)
+    self.room = destination
+    self.save!
+  end
+
+  def sees
+    room.description
+  end
 end
